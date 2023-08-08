87c4a0b4482cd50c8e4c9b9cb7d307f3b4df3677
Hiago <hiago-gil@hotmail.com>
2023-08-08 10:56:27 -0300

    massa de dados

--- cypress/e2e/endereco.cy.js
+++ cypress/e2e/endereco.cy.js
@@ -1,15 +1,39 @@
 /// <reference types="cypress" />
-
+import enderecoPage from '../support/page-objects/endereco.page';
+import EnderecoPage from '../support/page-objects/endereco.page'
+const dadosEndereco = require('../fixtures/endereco.json')
 describe('Funcionalidade Endereços - Faturamento e Entrega', () => {
-    
+
     beforeEach(() => {
         cy.visit('minha-conta')
-        cy.fixture('perfil').then(dados =>{
+        cy.wait(2000)
+        cy.fixture('perfil').then(dados => {
             cy.login(dados.usuario, dados.senha)
-    })
-        });
+        })
+    });
 
     it('Deve fazer cadastro de faturamento com sucesso', () => {
-
+        enderecoPage.editarEnderecoFaturamento('Hiago', 'Gil', 'Ebac', 'Brasil', 'Avenida Brasil', '50', 'Brasilia', 'Distrito Federal', '77777-777', '61999999999', 'hiago@teste.com')
+        cy.get('.woocommerce-message').should('contain', 'Endereço alterado com sucesso.')
     });
+
+    it.only('Deve fazer cadastro de faturamento com sucesso - usando arquivo de dados', () => {
+
+        enderecoPage.editarEnderecoFaturamento(
+            dadosEndereco[1].nome,
+            dadosEndereco[1].sobrenome,
+            dadosEndereco[1].empresa,
+            dadosEndereco[1].pais,
+            dadosEndereco[1].endereco,
+            dadosEndereco[1].numero,
+            dadosEndereco[1].cidade,
+            dadosEndereco[1].estado,
+            dadosEndereco[1].cep,
+            dadosEndereco[1].telefone,
+            dadosEndereco[1].email
+            ) 
+
+        cy.get('.woocommerce-message').should('contain', 'Endereço alterado com sucesso.')
+    });
+
 });
